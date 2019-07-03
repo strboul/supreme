6ee3d9c09f993d035d76bccba9d7e143c29a756f
Metin Yazici <stradivariusboul@gmail.com>
2019-07-03 22:51:26 +0200

    Example app with modules stub

---
+++ inst/examples/module-stock.R
@@ -0,0 +1,12 @@
+
+stock_page_module_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+stock_page_module_server <- function(input, output, session) {
+
+}
+
---
+++ inst/examples/module-transactions.R
@@ -0,0 +1,12 @@
+
+transactions_page_module_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+
+  )
+}
+
+transactions_page_module_server <- function(input, output, session) {
+
+}
+
