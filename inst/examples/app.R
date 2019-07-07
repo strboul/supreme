2546c4f9b1c0072bf8c577f3d056867160ad9e08
Metin Yazici <stradivariusboul@gmail.com>
2019-07-07 11:46:09 +0200

    Make the example app a bit more sophisticated

--- inst/examples/app.R
+++ inst/examples/app.R
@@ -1,17 +1,38 @@
 
 library(shiny)
+library(DT)
+library(ggplot2)
+
+sapply(list.files(pattern = "^module", full.names = TRUE), source)
 
 ui <- fluidPage(
-  customers_page_module_ui("CustomersPage"),
-  stock_page_module_ui("StockPage"),
-  transactions_page_module_ui("TransactionsPage")
+  titlePanel("Best clothes dashboard"),
+  tabsetPanel(id = "tabs",
+              tabPanel(title = "Items",
+                       value = "ItemsTab",
+                       items_tab_module_ui("ItemsTab")
+              ),
+              tabPanel(title = "Customers",
+                       value = "CustomersTab",
+                       customers_tab_module_ui("CustomersTab")
+              ),
+              tabPanel(title = "Transactions",
+                       value = "TransactionsTab",
+                       transactions_tab_module_ui("TransactionsTab")
+              ),
+              tabPanel(title = "About",
+                       value = "AboutTab",
+                       br(),
+                       "This is a purely fictional corporation."
+              )
+  )
 )
 
 server <- function(input, output, session) {
 
-  callModule(module = customers_page_module_server, id = "CustomersPage")
-  callModule(module = stock_page_module_server, id = "StockPage")
-  callModule(module = transactions_page_module_server, id = "TransactionsPage")
+  callModule(module = items_tab_module_server, id = "ItemsTab")
+  callModule(module = customers_tab_module_server, id = "CustomersTab")
+  callModule(module = transactions_tab_module_server, id = "TransactionsTab")
 
 }
 
--- inst/examples/module-transactions.R
+++ inst/examples/module-transactions.R
@@ -1,12 +1,26 @@
 
-transactions_page_module_ui <- function(id) {
+transactions_tab_module_ui <- function(id) {
   ns <- NS(id)
   tagList(
-
+    main_table_ui(ns("transactions_tbl")),
+    br()
   )
 }
 
-transactions_page_module_server <- function(input, output, session) {
+transactions_tab_module_server <- function(input, output, session) {
 
+  transactions.data <- reactive({
+    read.table(
+      file = file.path("data", "example_transactions.csv"),
+      sep = ",",
+      header = TRUE
+    )
+  })
+
+  callModule(
+    main_table_server,
+    "transactions_tbl",
+    transactions.data
+  )
 }
 
