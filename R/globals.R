1092c2c5a381ceee90c3c1e5a3740d794f17813a
Metin Yazici <stradivariusboul@gmail.com>
2019-07-18 23:00:09 +0200

    Create a file to keep all the global variables and define a few

---
+++ R/globals.R
@@ -0,0 +1,10 @@
+
+### ----------------------------------------------------------------- ###
+### PACKAGE GLOBAL VARIABLES ----
+### ----------------------------------------------------------------- ###
+
+#nocov start
+SUPREME.REQUIRED.FIELDS <- c("type", "name")
+SUPREME.OPTIONAL.FIELDS <- c("input", "output", "calling_modules", "src")
+#nocov end
+
