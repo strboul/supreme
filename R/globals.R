679d2e7ee6682a274c23433f8ea0b4e989cb5bb7
Metin Yazici <stradivariusboul@gmail.com>
2019-07-19 22:01:15 +0200

    Use snake_case notation for global variables

--- R/globals.R
+++ R/globals.R
@@ -4,7 +4,7 @@
 ### ----------------------------------------------------------------- ###
 
 #nocov start
-SUPREME.REQUIRED.FIELDS <- c("type", "name")
-SUPREME.OPTIONAL.FIELDS <- c("input", "output", "calling_modules", "src")
+SUPREME_REQUIRED_FIELDS <- c("type", "name")
+SUPREME_OPTIONAL_FIELDS <- c("input", "output", "calling_modules", "src")
 #nocov end
 
