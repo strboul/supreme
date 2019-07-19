2512208710138022f09f6eb97d51b7c620594ac8
Metin Yazici <stradivariusboul@gmail.com>
2019-07-19 22:12:44 +0200

    Implement yaml feature and create some yaml test data

---
+++ R/yaml.R
@@ -0,0 +1,95 @@
+
+#' Read yaml model
+#'
+#' @param path file path to a yaml file.
+#' @param verify should the loaded yaml list be verified against the supreme object
+#'   model?
+#'
+#' @examples
+#' file <- file.path("yaml-test", "example-model-1.yaml")
+#' supreme(src_yaml(file))
+#'
+#' @importFrom yaml yaml.load_file
+#' @export
+src_yaml <- function(path, verify = TRUE) {
+  obj <- yaml::yaml.load_file(path)
+  if (verify) {
+    verify_yaml(obj)
+  }
+  structure(obj, class = "src_yaml")
+}
+
+#' @export
+print.src_yaml <- function(x, ...) {
+  cat("Model yaml object", "\n")
+}
+
+#' Verify yaml object for supreme
+#'
+#' The loaded yaml model can be verified according to the structure of an supreme
+#' object model. The errors catched during the parsing of yaml file will be handled
+#' by the *yaml* package.
+#'
+#' @param x a list (yaml) object.
+#'
+#' @details
+#'
+#' + Checks whether yaml object contains sub-lists
+#'
+#' + Checks whether yaml object does miss some or all required fields
+#'
+#' + Checks whether yaml object contains any other field not existing in either
+#' required or optional fields
+#'
+#' @return returns (invisibly) true if everything is fine.
+#' @noRd
+verify_yaml <- function(x) {
+  if (!is_list(x)) {
+    ncstopf("cannot verify object with a class of: '%s'", class(x))
+  }
+  if (!is.null(names(x))) {
+    ncstopf("malformed yaml model")
+  }
+  required <- SUPREME_REQUIRED_FIELDS
+  optional <- SUPREME_OPTIONAL_FIELDS
+  for (xi in seq_along(x)) {
+    nst <- vapply(seq_along(x[[xi]]), function(zi) {
+      is.list(x[[xi]][[zi]])
+    }, logical(1))
+    anst <- any(nst)
+    if (anst) {
+      ncstopf(paste(
+        "model yaml cannot contain sub-list in",
+        paste0("'", xi, "'"),
+        "element:", paste("'", names(x[[xi]])[nst], "'", sep = "", collapse = ", ")
+      ))
+    }
+    anst
+  }
+  for (i in seq_along(x)) {
+    elem <- x[[i]]
+    required.names <- required %in% names(elem)
+    if (!all(required.names)) {
+      ncstopf(
+        paste(
+          "required fields are missing in",
+          paste0("'", i,"'"),
+          "element:",
+          paste("'", required[!required.names], "'", sep = "", collapse = ", ")
+        )
+      )
+    }
+    all.names <- c(required, optional)
+    req.opt.names <- names(elem) %in% all.names
+    if (!all(req.opt.names)) {
+      ncstopf(
+        paste(
+          "the following names not required or optional:",
+          paste("'", names(elem)[!req.opt.names], "'", sep = "", collapse = ", ")
+        )
+      )
+    }
+  }
+  invisible(TRUE)
+}
+
