442dca35b3599244bd4948a63b214cc44fb360c2
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-08-03 23:07:18 +0200

    Modularize `src_file()`

--- R/src-file.R
+++ R/src-file.R
@@ -1,5 +1,7 @@
 
-#' Read an R file containing Shiny application
+#' Read \R files
+#'
+#' The files should contain some Shiny application.
 #'
 #' @param x a file path.
 #' @examples
@@ -9,8 +11,9 @@
 src_file <- function(x) {
   tryCatch({ file.exists(as.character(x)) }, error = function(e)
     ncstopf("cannot read file: %s", conditionMessage(e)))
-  obj <- make_module_entities(x)
-  structure(obj, class = "src_file")
+  obj <- make_module_entities_from_paths(x)
+  out <- entity_constructor(obj)
+  structure(out, class = "src_file")
 }
 
 #' @export
@@ -48,37 +51,17 @@ read_srcfile <- function(x) {
   parse(text = lines)
 }
 
-#' Workhorse for the `src_file()` call
+#' Make module entities from file paths
 #'
 #' @param x file paths.
 #' @noRd
-make_module_entities <- function(x) {
-  modules <- lapply(seq_along(x), function(i) {
+make_module_entities_from_paths <- function(x) {
+  out <- lapply(seq_along(x), function(i) {
     path <- x[i]
     body <- read_srcfile(path)
     src <- path
     list(body = body, src = src)
   })
-  fields <- c(SUPREME_REQUIRED_FIELDS, SUPREME_OPTIONAL_FIELDS)
-  res <- list()
-  for (i in seq_along(modules)) {
-    entity <- modules[[i]]
-    src <- entity[["src"]]
-    entity.body <- entity[["body"]][[1]]
-    which.components <- which(sapply(entity.body, is_shiny_server_component))
-    for (c in which.components) {
-      f.body <- entity.body[[c]]
-      name <- find_block_assignment_name(f.body)
-      calling_modules <- find_block_calling_modules(f.body)
-      out <- list(
-        type = "module",
-        name = name,
-        calling_modules = calling_modules,
-        src = src
-      )
-      res[[length(res)+1L]] <- out
-    }
-  }
-  res
+  structure(out, class = "module_entities")
 }
 
