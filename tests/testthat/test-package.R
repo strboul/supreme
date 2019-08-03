3f5d6fa80edf5ba8dbc0ab91da9141d360fb7382
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-08-03 23:55:38 +0200

    Create `example_package()` (mainly derived from the remnants of the test in `test-package.R`)

--- tests/testthat/test-package.R
+++ tests/testthat/test-package.R
@@ -2,34 +2,9 @@
 context("test-package")
 
 test_that("can read package 'supreme.pkg.test'", {
-  pkg.name <- "supreme.pkg.test"
+  pkg <- example_package()
 
-  if (!supreme:::is_package_exist(pkg.name)) {
-    pkg.path <- file.path("testpackages", pkg.name)
-    answer <- menu(
-      choices = c("Yes", "No"),
-      title = paste(
-        "\n",
-        paste0(
-          "The test package '",
-          pkg.name,
-          "' not found in the system."),
-        "Do you want to install it to run the tests?",
-        "",
-        paste0(
-          "The package placed under the directory: '",
-          tools::file_path_as_absolute(pkg.path),
-          "'"
-        ),
-        sep = "\n"
-      )
-    )
-    if (identical(answer, 1L)) {
-      devtools::install(pkg.path)
-    }
-  }
-
-  sourced <- src_pkg(pkg.name)
+  sourced <- src_pkg(pkg)
 
   expect_equal(sourced,
                structure(list(
