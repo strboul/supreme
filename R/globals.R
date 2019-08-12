1049f71eb69aa754feaa797f8435bbe2e24bbb4b
Metin Yazici <stradivariusboul@gmail.com>
2019-08-12 22:53:46 +0200

    Make R CMD happy

--- R/globals.R
+++ R/globals.R
@@ -6,5 +6,9 @@
 #nocov start
 SUPREME_REQUIRED_FIELDS <- c("type", "name")
 SUPREME_OPTIONAL_FIELDS <- c("input", "output", "calling_modules", "src")
+
+## Variables to prevent R CMD notes
+utils::globalVariables(c("callModule", "moduleA_server"))
+
 #nocov end
 
