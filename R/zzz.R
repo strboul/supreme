1a02f67f940ca23c3033eacbb68a778696c0f682
Metin Yazici <25015317+strboul@users.noreply.github.com>
2019-08-20 21:14:48 +0200

    Fix assignment operator in options list

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@
 .onLoad <- function(libname, pkgname) {
   op <- options()
   op_supreme <- list(
-    SUPREME_MODEL_REQUIRED_FIELDS <- c("type", "name"),
-    SUPREME_MODEL_OPTIONAL_FIELDS <- c("input", "output", "calling_modules", "src")
+    SUPREME_MODEL_REQUIRED_FIELDS = c("type", "name"),
+    SUPREME_MODEL_OPTIONAL_FIELDS = c("input", "output", "calling_modules", "src")
   )
   toset <- !(names(op_supreme) %in% names(op))
   if(any(toset)) options(op_supreme[toset])
