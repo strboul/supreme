898861c84400be3db0bf163cdb7f4412d03aa778
Metin Yazici <stradivariusboul@gmail.com>
2019-11-23 21:31:20 +0100

    Move contents of module env and pkg

--- R/src-methods.R
+++ R/src-methods.R
@@ -100,15 +100,16 @@ src_expr <- function(x) {
 #' The package should contain some Shiny application.
 #'
 #' @param x a package name as character (must be installed in the system).
+#' @details
+#' The function coerces input to be a character.
 #' @family source functions
 #' @export
 src_pkg <- function(x) {
-  ## enforce input to be a character.
   if (!is.character(x)) x <- as.character(x)
   if (!is_package_exist(x)) {
     ncstopf("package '%s' not found.", x)
   }
-  obj <- make_src_pkg(x)
+  obj <- .make_src_pkg(x)
   out <- entity_constructor(obj)
   structure(out, class = c("src_obj", "src_pkg"))
 }
@@ -123,7 +124,7 @@ src_env <- function(x) {
   if (!is.environment(x)) {
     ncstopf("input must be environment, not:", typeof(x))
   }
-  obj <- make_src_env(x)
+  obj <- .make_src_env(x)
   out <- entity_constructor(obj)
   structure(out, class = c("src_obj", "src_env"))
 }
@@ -307,5 +308,67 @@ print.src_env <- function(x, ...) {
 .make_module_entities_from_expression <- function(x) {
   out <- list(list(body = x, src = NULL))
   structure(out, class = "module_entities")
+}
+
+
+#' Make module entities from packages
+#'
+#' (Packages are essentially environments)
+#'
+#' @importFrom utils packageName
+#' @noRd
+.make_src_pkg <- function(pkg.name) {
+  stopifnot(is.character(pkg.name))
+  ns <- asNamespace(pkg.name)
+  ## verify package name
+  stopifnot(length(utils::packageName(asNamespace(pkg.name))) > 0L)
+  entities <- .make_module_entities_from_environment(ns)
+  out <- list(list(body = entities, src = paste("package", pkg.name, sep = ":")))
+  structure(out, class = "module_entities")
+}
+
+
+#' Make module entities from environment objects
+#'
+#' @noRd
+.make_src_env <- function(envir) {
+  stopifnot(is.environment(envir))
+  entities <- .make_module_entities_from_environment(envir)
+  out <- list(list(body = entities, src = "expression"))
+  structure(out, class = "module_entities")
+}
+
+
+#' Make module entities from an environment
+#'
+#' @param x an environment.
+#' @details
+#' This call does not take all objects from an environment. Objects from a given
+#' environment will be eliminated:
+#' 1. if it is not a function,
+#' 2. (it is a function), but if it is not a Shiny component.
+#' @noRd
+.make_module_entities_from_environment <- function(x) {
+  stopifnot(is.environment(x))
+  objs <- mget(ls(x, all.names = TRUE), envir = x)
+  funs <- Filter(is.function, objs)
+  server_components <- Filter(is_shiny_server_component, funs)
+  exprs <- vector("list", length(server_components))
+  for (i in seq_along(server_components)) {
+    elem <- server_components[i]
+    elem.name <- names(elem)
+    elem.sub <- elem[[1L]]
+    Arguments <- formals(elem.sub)
+    Body <- body(elem.sub)
+    Function <- substitute({name <- fun}, list(
+      name = as.name(elem.name),
+      fun = as.function(x = c(Arguments, Body))
+    ))[[2L]]
+    # TODO maybe tlist?
+    exprs[[length(exprs) + 1L]] <- Function
+  }
+  exprs <- as.call(c(as.name("{"), exprs))
+  exprs <- as.expression(exprs)
+  exprs
 }
 
